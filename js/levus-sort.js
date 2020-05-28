0b6be56699e5f50e5a8d0ba4bf828d4e79d987ec
lvivduncan <lvivduncan@gmail.com>
2020-05-28 15:20:03 +0300

    start

---
+++ js/levus-sort.js
@@ -0,0 +1,30 @@
+// jquery ^_^
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+// кнопки перемикання
+const buttons = $('#levus-buttons span');
+
+// основний блок з елементами
+const wrapper = $('#levus-items');
+
+// елементи
+const items = $('#levus-items figure');
+
+// перемикаємо
+buttons[0].addEventListenter('click', getName);
+buttons[1].addEventListenter('click', getPrice);
+buttons[2].addEventListenter('click', getSize);
+
+// функції
+function getName(){
+
+}
+
+function getPrice(){
+
+}
+
+function getSize(){
+
+}
