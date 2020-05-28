8fc117d90b5e11089fd5fa41a6e800fbd2ee79c2
lvivduncan <lvivduncan@gmail.com>
2020-05-28 20:24:35 +0300

    optimization

--- js/levus-sort.js
+++ js/levus-sort.js
@@ -1,71 +1,45 @@
-// jquery ^_^
-const $ = document.querySelector.bind(document);
-const $$ = document.querySelectorAll.bind(document);
-
 // кнопки перемикання
-const buttons = $$('#levus-buttons span');
+const buttons = document.querySelectorAll('#levus-buttons span');
 
 // основний блок з елементами
-const wrapper = $('#levus-items');
+const wrapper = document.querySelector('#levus-items');
 
 // елементи
-const items = $$('#levus-items figure');
+const items = document.querySelectorAll('#levus-items figure');
 
 // перемикаємо
 buttons[0].addEventListener('click', sortByName);
 buttons[1].addEventListener('click', sortByPrice);
 buttons[2].addEventListener('click', sortBySize);
 
-// сортуємо по імені
-function sortByName() {
+// 1 функція для всіх
+function sort(number){
   const values = [];
-  items.forEach(item => {
-    values.push(item.dataset.item.split(',')[0]);
-  });
+
+  items.forEach(item => values.push(item.dataset.item.split(',')[number]));
 
   values.sort();
 
-  items.forEach((item, i) => {
+  items.forEach(item => {
     values.forEach((val, num) => {
-      if (val == item.dataset.item.split(',')[0]) {
-        item.style.order = values.indexOf(values[num])
+      if (val == item.dataset.item.split(',')[number]) {
+        item.style.order = values.indexOf(values[num]);
       }
     });
   });
+}
+
+// сортуємо по імені
+function sortByName() {
+  sort(0);
 }
 
 // сортуємо за ціною
 function sortByPrice() {
-  const values = [];
-  items.forEach(item => {
-    values.push(item.dataset.item.split(',')[1]);
-  });
-
-  values.sort((a, b) => a - b);
-
-  items.forEach((item, i) => {
-    values.forEach((val, num) => {
-      if (val == item.dataset.item.split(',')[1]) {
-        item.style.order = values.indexOf(values[num])
-      }
-    });
-  });
+  sort(1);
 }
 
 // сортуємо за розміром
 function sortBySize() {
-  const values = [];
-  items.forEach(item => {
-    values.push(item.dataset.item.split(',')[2]);
-  });
-
-  values.sort((a, b) => a - b);
-
-  items.forEach((item, i) => {
-    values.forEach((val, num) => {
-      if (val == item.dataset.item.split(',')[2]) {
-        item.style.order = values.indexOf(values[num])
-      }
-    });
-  });
+  sort(2);
 }
